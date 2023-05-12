9dd6609b42fbed59b90008ed8ec4176fb80b3b9b
Gennadiy Semenduyev <gsemenduyev@freewheel.tv>
2023-05-12 10:38:03 -0500

    Delete E2E/Stratasphere directory

--- E2E/Stratasphere/cypress/support/page-objects/agency-pages/SearchRfpPage.js
+++
@@ -1,9 +0,0 @@
-class SearchRfpPage {
-    rfpNameTextBox() {
-        return cy.get("[placeholder='RFP Name']");
-    }
-    searchButton() {
-        return cy.get('.btn.btn-primary.btn-sm').eq(1);
-    }
-}
-export const searchRfpPage = new SearchRfpPage;
--- E2E/Stratasphere/cypress/support/page-objects/ssphere-pages/SSphereHomePage.js
+++
@@ -1,9 +0,0 @@
-class SSphereHomePage{
-    proposalsField() {
-        return cy.get('a[ui-sref="proposals"]');
-    }
-    proposalsFieldSelectorSyntax() {
-        return 'a[ui-sref="proposals"]';
-    }
-}
-export const sSphereHomePage = new SSphereHomePage;
