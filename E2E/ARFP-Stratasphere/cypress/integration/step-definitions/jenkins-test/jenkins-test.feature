161ef332cbdbfe02609c1fcccf71c6fb966d3174
gsemenduyev <gsemenduyev@freewheel.com>
2023-06-14 22:46:34 -0500

    Added Jenkinsfile

---
+++ E2E/ARFP-Stratasphere/cypress/integration/step-definitions/jenkins-test/jenkins-test.feature
@@ -0,0 +1,7 @@
+@wip
+Feature: Workflow between buyer and seller Jenkins test
+
+
+  Scenario: Buyer creates new RFP and send it to Stratashere (WBBS)
+    * Login to Agency RFP Jenkins test
+    * Logout Agency RFP Jenkins test
---
+++ E2E/ARFP-Stratasphere/cypress/integration/step-definitions/jenkins-test/jenkins-test.js
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />
+import { Given } from "@badeball/cypress-cucumber-preprocessor";
+import AgencyLoginPage from "../../../support/page-objects/agency-pages/AgencyLoginPage";
+import AgencyBasePage from "../../../support/page-objects/agency-pages/AgencyBasePage";
+const agencyLoginPage = new AgencyLoginPage;
+const agencyBasePage = new AgencyBasePage;
+// Login to Agency RFP Jenkins test
+Given('Login to Agency RFP Jenkins test', () => {
+    cy.visit(Cypress.env('agencyUrl') + "/Login");
+    agencyBasePage.pageTitle().should('have.text', 'Sign In');
+    agencyLoginPage.usernameBox().type(Cypress.env('agencyUsername'));
+    agencyLoginPage.passwordBox().type(Cypress.env('agencyPassword'), { log: false });
+    agencyLoginPage.loginButton().click();
+    cy.title().should('eq', 'Home - RFP');
+})
+
+// Logout Agency RFP Jenkins test
+Given('Logout Agency RFP Jenkins test', () => {
+    agencyBasePage.signOutButton().click({ force: true });
+    agencyBasePage.pageTitle().should('have.text', 'Sign In');
+})
