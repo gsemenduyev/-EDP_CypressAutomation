94b891dfb5fd0c625c2bd9e63140fda6ba9dd526
gsemenduyev <gsemenduyev@freewheel.com>
2023-11-09 17:20:03 -0600

    Created Strata eBiz folder

---
+++ E2E/Strata-eBiz/cypress/integration/step-definitions/strata-ebiz/strata-ebiz.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+import { Given } from "@badeball/cypress-cucumber-preprocessor";
+
+
+Given('Login to ARFP {string} environment', environment => { })
+Given('Validate ARFP home page', () => { })
+Given('Logout Agency RFP', () => { })
+Given('Login to Stratasphere {string} environment', environment => { })
+Given('Validate Stratasphere home page', () => { })
+Given('Logout from Stratasphere', () => { })
+Given('Login to sTraffic {string} environment', environment => { })
+Given('Validate sTraffic home page', () => { })
+Given('Logout from sTraffic', () => { })
+Given('Login to Eleven {string} environment', environment => { })
+Given('Validate Eleven home page', () => { })
+Given('Logout from Eleven', () => { })
