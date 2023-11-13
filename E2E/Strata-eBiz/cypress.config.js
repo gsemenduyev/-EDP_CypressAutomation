79ba24a11ac83b81099594a68e4e140aa7e34fd3
gsemenduyev <gsemenduyev@freewheel.com>
2023-11-13 16:04:26 -0600

    Updated Jenkinsfile 6

--- E2E/Strata-eBiz/cypress.config.js
+++ E2E/Strata-eBiz/cypress.config.js
@@ -75,8 +75,8 @@ async function setupNodeEvents(cypressOn, config) {
 module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
-  defaultCommandTimeout: 10000,
-  pageLoadTimeout: 60000,
+  defaultCommandTimeout: 30000,
+  pageLoadTimeout: 300000,
   screenshotOnRunFailure: true,
   video: true,
   retries: {
