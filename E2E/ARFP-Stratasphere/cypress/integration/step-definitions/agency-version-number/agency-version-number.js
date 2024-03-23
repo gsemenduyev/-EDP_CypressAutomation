96ffcaa282e3b0e2b91983ce3663c396a93a5047
gsemenduyev <gsemenduyev@freewheel.com>
2024-03-23 02:01:30 -0500

    Q11

--- E2E/ARFP-Stratasphere/cypress/integration/step-definitions/agency-version-number/agency-version-number.js
+++ E2E/ARFP-Stratasphere/cypress/integration/step-definitions/agency-version-number/agency-version-number.js
@@ -20,22 +20,41 @@ Given('Validate Agency RFP version number', () => {
     })
 })
 
-Given('Test', () => {
-    const commands = [
-        'cd C:\\Program Files (x86)\\SmartBear\\TestExecute 15\\Bin',
-        'TestComplete.exe "C:\\CypressAutomation\\EDP_CypressAutomation\\E2E\\SBMS\\SBMS.pjs" /r /p:SBMS /t:"KeywordTests|OpenSBMS" /e'
-    ];
 
-    cy.exec(commands.join(' && '), { timeout: 1000000 }).then((result) => {
+Given('Test', () => {
+    const command = `
+      source ~/.bash_profile > /dev/null 2>&1
+      cd "C:/Program Files (x86)/SmartBear/TestExecute 15/Bin"
+      TestComplete.exe "C:/CypressAutomation/EDP_CypressAutomation/E2E/SBMS/SBMS.pjs" /r /p:SBMS /t:"KeywordTests|OpenSBMS" /e
+    `;
+
+    cy.exec(command, { timeout: 1000000 }).then((result) => {
         if (result.stderr) {
             // Handle the error
             cy.log(`Error: ${result.stderr}`);
         } else {
             // Use the output
-            cy.log(`Commands output:\n${result.stdout}`);
+            cy.log(`Command output:\n${result.stdout}`);
         }
     });
 });
+
+// Given('Test', () => {
+//     const commands = [
+//         'cd C:\\Program Files (x86)\\SmartBear\\TestExecute 15\\Bin',
+//         'TestComplete.exe "C:\\CypressAutomation\\EDP_CypressAutomation\\E2E\\SBMS\\SBMS.pjs" /r /p:SBMS /t:"KeywordTests|OpenSBMS" /e'
+//     ];
+
+//     cy.exec(commands.join(' && '), { timeout: 1000000 }).then((result) => {
+//         if (result.stderr) {
+//             // Handle the error
+//             cy.log(`Error: ${result.stderr}`);
+//         } else {
+//             // Use the output
+//             cy.log(`Commands output:\n${result.stdout}`);
+//         }
+//     });
+// });
 
 Given('Test 2', () => {
     cy.visit('https://www.homedepot.com/').screenshot()
