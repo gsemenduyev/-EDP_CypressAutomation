2678aaacc1b8f33b7240db915d9f43128e278ffd
gsemenduyev <gsemenduyev@freewheel.com>
2024-03-22 23:36:25 -0500

    AddedJenkins

--- E2E/ARFP-Stratasphere/cypress/integration/step-definitions/agency-version-number/agency-version-number.feature
+++ E2E/ARFP-Stratasphere/cypress/integration/step-definitions/agency-version-number/agency-version-number.feature
@@ -5,6 +5,26 @@ Feature: Validate Agency RFP version number
     Agent login to Agency RFP and validate version number
 
     Scenario: User logins to Agency RFP and verifies app version number
-        * Login to Agency RFP with 'Permanent' password
-        * Validate Agency RFP version number
-        * Logout Agency RFP
+        # * Login to Agency RFP with 'Permanent' password
+        # * Validate Agency RFP version number
+        # * Logout Agency RFP
+        * Test
+
+    Scenario: Open youtube
+        # * Login to Agency RFP with 'Permanent' password
+        # * Validate Agency RFP version number
+        # * Logout Agency RFP
+        * Test 2
+
+
+    Scenario: User logins to Agency RFP and verifies app version number 2
+        # * Login to Agency RFP with 'Permanent' password
+        # * Validate Agency RFP version number
+        # * Logout Agency RFP
+        * Test
+
+    Scenario: Open youtube 2
+        # * Login to Agency RFP with 'Permanent' password
+        # * Validate Agency RFP version number
+        # * Logout Agency RFP
+        * Test 2
--- E2E/ARFP-Stratasphere/cypress/integration/step-definitions/agency-version-number/agency-version-number.js
+++ E2E/ARFP-Stratasphere/cypress/integration/step-definitions/agency-version-number/agency-version-number.js
@@ -12,10 +12,33 @@ const VERSION = Cypress.env('VERSION')
 // Validate Agency RFP version number
 Given('Validate Agency RFP version number', () => {
     agencyBasePage.versionNumber().invoke('text').then(version => {
-        if(VERSION === undefined || VERSION === null){
+        if (VERSION === undefined || VERSION === null) {
             throw new Error(`Set Agency RFP version ${version.trim()} as Environment veritable.`)
         } else {
-            expect(version.trim()).to.equal(VERSION) 
+            expect(version.trim()).to.equal(VERSION)
         }
     })
+})
+
+Given('Test', () => {
+    const commands = [
+        'cd C:\\Program Files (x86)\\SmartBear\\TestComplete 15\\Bin',
+        'TestComplete.exe "C:\\CypressAutomation\\EDP_CypressAutomation\\E2E\\SBMS\\SBMS.pjs" /r /p:SBMS /t:' +
+        '"KeywordTests|OpenSBMS"' +
+        ' /e'
+    ];
+
+    cy.exec(commands.join(' && '), { timeout: 1000000 }).then((result) => {
+        if (result.stderr) {
+            // Handle the error
+            cy.log(`Error: ${result.stderr}`);
+        } else {
+            // Use the output
+            cy.log(`Commands output:\n${result.stdout}`);
+        }
+    });
+});
+
+Given('Test 2', () => {
+    cy.visit('https://www.homedepot.com/').screenshot()
 })
