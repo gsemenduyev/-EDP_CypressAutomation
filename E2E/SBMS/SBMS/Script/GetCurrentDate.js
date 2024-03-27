117ec4882f19d4151980e0848ad0218d8bfd5968
gsemenduyev <gsemenduyev@freewheel.com>
2024-03-27 18:46:58 -0500

    Added keyword tests

---
+++ E2E/SBMS/SBMS/Script/GetCurrentDate.js
@@ -0,0 +1,8 @@
+﻿function getCurrentDate(){
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+return mm+'/'+dd+'/'+yyyy;
+}
+module.exports.getCurrentDate = getCurrentDate;
