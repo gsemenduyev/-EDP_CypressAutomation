8624162479cb37e8318de006b3bcfc2e3e2cc815
gsemenduyev <gsemenduyev@freewheel.com>
2024-07-11 23:18:39 -0500

    Part 2

--- E2E/SBMS/SBMS/Script/ReadWriteFile.js
+++ E2E/SBMS/SBMS/Script/ReadWriteFile.js
@@ -19,8 +19,9 @@ Writes to Text file from Stores Files
  Accepts - parameter filename, content
  **/
 function writeStoreTxtFile(fileName, content) {
+  clearFileContent(fileName);
   try {
-    var filePath = Project.Path + "Stores\\Files\\" + fileName;
+    var filePath = Project.Path + "\\Stores\\Files\\" + fileName;
     var file = aqFile.OpenTextFile(filePath, aqFile.faWrite, aqFile.ctANSI);
     file.Write(content);
     file.Close();
@@ -29,3 +30,37 @@ function writeStoreTxtFile(fileName, content) {
     Log.Error("An error occurred: " + e.message);
   }
 }
+
+
+/* 
+Writes to Text file from Stores Files
+ Accepts - parameter filename, content
+ **/
+function clearFileContent(fileName) {
+  var filePath = Project.Path + "\\Stores\\Files\\" + fileName;
+  // Check if the file exists
+  if (aqFile.Exists(filePath)) {
+    try {
+      // Delete the file
+      aqFile.Delete(filePath);
+      // Create a new empty file
+      aqFile.Create(filePath);
+    } catch (e) {
+      Log.Error("Failed to clear file content by recreating the file: " + e.message);
+    }
+
+    // Verify the content is cleared
+    var readFile = aqFile.OpenTextFile(filePath, aqFile.faRead, aqFile.ctANSI);
+    var content = readFile.ReadAll();
+    readFile.Close();
+    if (content === "") {
+      Log.Message("The file " + fileName + " content is cleared.");
+    } else {
+      Log.Error("The file " + fileName + " content is not cleared.");
+    }
+  } else {
+    Log.Error("File not found: " + filePath);
+  }
+}
+
+
