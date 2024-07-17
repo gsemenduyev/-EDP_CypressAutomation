0090a666fd3c709f16e07583a2c698e492f0406f
gsemenduyev <gsemenduyev@freewheel.com>
2024-07-17 17:28:36 -0500

    Finished UAT

--- E2E/SBMS/SBMS/Script/ReadWriteFile.js
+++ E2E/SBMS/SBMS/Script/ReadWriteFile.js
@@ -6,11 +6,17 @@ Reads Text file from Stores Files
 function readStoreTxtFile(fileName){
   try {
     var filePath = Project.Path + "\\Stores\\Files\\" + fileName;
-    var fileText = aqFile.ReadWholeTextFile(filePath, aqFile.ctANSI);
-    Log.Message(fileText + "-Environment set up by Cypress test.");
-    return fileText;
+    if (aqFile.Exists(filePath)) {
+      var fileText = aqFile.ReadWholeTextFile(filePath, aqFile.ctANSI);
+      Log.Message(fileText + "-Environment set up by Cypress test.");
+      return fileText;
+    } else {
+      Log.Message("File not found: " + filePath);
+      return "";
+    }
   } catch (e) {
     Log.Error("An error occurred: " + e.message);
+    return "";
   }
 }
 
@@ -34,33 +40,31 @@ function writeStoreTxtFile(fileName, content) {
 
 /* 
 Clears content in Text file from Stores Files
- Accepts - parameter filename, content
+ Accepts - parameter filename
  **/
 function clearFileContent(fileName) {
   var filePath = Project.Path + "\\Stores\\Files\\" + fileName;
-  // Check if the file exists
-  if (aqFile.Exists(filePath)) {
-    try {
+  try {
+    // Check if the file exists
+    if (aqFile.Exists(filePath)) {
       // Delete the file
       aqFile.Delete(filePath);
-      // Create a new empty file
-      aqFile.Create(filePath);
-    } catch (e) {
-      Log.Error("Failed to clear file content by recreating the file: " + e.message);
+      Log.Message("File " + fileName + " deleted.");
     }
+    // Create a new empty file
+    aqFile.Create(filePath);
+    Log.Message("File " + fileName + " created.");
+  } catch (e) {
+    Log.Error("Failed to clear file content by recreating the file: " + e.message);
+  }
 
-    // Verify the content is cleared
-    var readFile = aqFile.OpenTextFile(filePath, aqFile.faRead, aqFile.ctANSI);
-    var content = readFile.ReadAll();
-    readFile.Close();
-    if (content === "") {
-      Log.Message("The file " + fileName + " content is cleared.");
-    } else {
-      Log.Error("The file " + fileName + " content is not cleared.");
-    }
+  // Verify the content is cleared
+  var readFile = aqFile.OpenTextFile(filePath, aqFile.faRead, aqFile.ctANSI);
+  var content = readFile.ReadAll();
+  readFile.Close();
+  if (content === "") {
+    Log.Message("The file " + fileName + " content is cleared.");
   } else {
-    Log.Error("File not found: " + filePath);
+    Log.Error("The file " + fileName + " content is not cleared.");
   }
 }
-
-
