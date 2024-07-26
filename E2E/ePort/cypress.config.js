ca3b062252c778e4702212114aebe4fe0c977a3d
gsemenduyev <gsemenduyev@freewheel.com>
2024-07-26 15:37:54 -0500

    Z24

--- E2E/ePort/cypress.config.js
+++ E2E/ePort/cypress.config.js
@@ -123,6 +123,7 @@ module.exports = defineConfig({
   trashAssetsBeforeRuns: false,
   video: true,
   chromeWebSecurity: false,
+  userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.185",
   retries: {
     runMode: 0,
     openMode: 0
