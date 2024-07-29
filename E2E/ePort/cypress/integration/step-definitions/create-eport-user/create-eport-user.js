d1f00edb29bb42b7074ec61ec4b67baa258371dc
gsemenduyev <gsemenduyev@freewheel.com>
2024-07-29 12:18:15 -0500

    A1

--- E2E/ePort/cypress/integration/step-definitions/create-eport-user/create-eport-user.js
+++ E2E/ePort/cypress/integration/step-definitions/create-eport-user/create-eport-user.js
@@ -4,98 +4,107 @@ import { Given } from "@badeball/cypress-cucumber-preprocessor";
 import EPortHomePage from "../../../support/page-objects/eport-pages/EPortHomePage";
 import EPortOrgAdminPage from "../../../support/page-objects/eport-pages/EPortOrgAdminPage";
 
-const ePortHomePage = new EPortHomePage;
-const ePortOrgAdminPage = new EPortOrgAdminPage;
+const ePortHomePage = new EPortHomePage();
+const ePortOrgAdminPage = new EPortOrgAdminPage();
 
 const RANDOM = Math.floor(Math.random() * (1000000, 9999999)) + 1000000;
-const FIRST_NAME = 'Test';
+const FIRST_NAME = "Test";
 const LAST_NAME = `User${RANDOM}`;
-const PHONE_NUMBER = '(123) 456-7891';
+const PHONE_NUMBER = "(123) 456-7891";
 const PRIMARY_EMAIL = `${FIRST_NAME}${LAST_NAME}@mailinator.com`;
-const USER_TYPE = 'Org Admin Seller';
+const USER_TYPE = "Org Admin Seller";
 const LOGIN_NAME = `${FIRST_NAME}${LAST_NAME}`;
-const NEW_PASSWORD = 'password1';
+const NEW_PASSWORD = "password1";
 const ORGANIZATION = (() => {
-    if (Cypress.env('ENV') === 'Production') {
-        return 'WTVB (S)';
-    } else if (Cypress.env('ENV') === 'UAT') {
-        return 'TEST (S)'
-    } else {
-        return 'KDVR (S)';
-    }
+  if (Cypress.env("ENV") === "Production") {
+    return "WTVB (S)";
+  } else if (Cypress.env("ENV") === "UAT") {
+    return "TEST (S)";
+  } else {
+    return "KDVR (S)";
+  }
 })();
 
-Given('Navigate to ePort Org Admin Page', () => {
-    ePortHomePage.selectActionDropdown().select('Org Admin');
-    cy.title().should('eq', 'ePort - Administration');
-    cy.screenshot();
-    ePortOrgAdminPage.title().should('have.text', 'Administration');
-    ePortOrgAdminPage.tabUserMgr().as('tabUserMgr').click();
-    cy.get('@tabUserMgr')
-        .should('have.text', 'User Manager')
-        .should('have.class', 'selectedtabbutton');
+Given("Navigate to ePort Org Admin Page", () => {
+  ePortHomePage.selectActionDropdown().select("Org Admin");
+  cy.title().should("eq", "ePort - Administration");
+  cy.screenshot();
+  ePortOrgAdminPage.title().should("have.text", "Administration");
+  ePortOrgAdminPage.tabUserMgr().as("tabUserMgr").click();
+  cy.get("@tabUserMgr")
+    .should("have.text", "User Manager")
+    .should("have.class", "selectedtabbutton");
 });
 
-Given('Verify Org Admin page User Manager grid', () => {
-    ePortOrgAdminPage.userMgrGridHeaders()
-        .should('contain.text', 'First Name')
-        .should('contain.text', 'Last Name')
-        .should('contain.text', 'Login Name')
-        .should('contain.text', 'Organizations')
-        .should('contain.text', 'Office')
-        .should('contain.text', 'User Type')
-        .should('contain.text', 'Phone')
-        .should('contain.text', 'Email')
-        .should('contain.text', 'Active')
-        .should('contain.text', 'Last Updated');
-    cy.screenshot();
+Given("Verify Org Admin page User Manager grid", () => {
+  ePortOrgAdminPage
+    .userMgrGridHeaders()
+    .should("contain.text", "First Name")
+    .should("contain.text", "Last Name")
+    .should("contain.text", "Login Name")
+    .should("contain.text", "Organizations")
+    .should("contain.text", "Office")
+    .should("contain.text", "User Type")
+    .should("contain.text", "Phone")
+    .should("contain.text", "Email")
+    .should("contain.text", "Active")
+    .should("contain.text", "Last Updated");
+  cy.screenshot();
 });
 
-Given('Create new ePort user', () => {
-    ePortOrgAdminPage.userMgrFirstName().type(FIRST_NAME);
-    ePortOrgAdminPage.userMgrLastName().type(LAST_NAME);
-    ePortOrgAdminPage.userMgrPhone().type(PHONE_NUMBER);
-    ePortOrgAdminPage.userMgrEmail().type(PRIMARY_EMAIL);
-    ePortOrgAdminPage.userMgrTypeDropdown().select(USER_TYPE);
-    ePortOrgAdminPage.userMgrLoginName().type(LOGIN_NAME);
-    ePortOrgAdminPage.userMgrNewPass().type(NEW_PASSWORD);
-    ePortOrgAdminPage.userMgrAvailOrgs().select(ORGANIZATION);
-    ePortOrgAdminPage.userMgrAddBtn().click();
-    ePortOrgAdminPage.userMgrSelectedOrgs().should('contain.text', ORGANIZATION);
-    ePortOrgAdminPage.userMgrCreateBtn().click();
-    cy.screenshot();
-    ePortOrgAdminPage.userMgrMessage().should('have.text', `User ${LOGIN_NAME} has been created successfully.`);
-    cy.screenshot();
+Given("Create new ePort user", () => {
+  ePortOrgAdminPage.userMgrFirstName().type(FIRST_NAME);
+  ePortOrgAdminPage.userMgrLastName().type(LAST_NAME);
+  ePortOrgAdminPage.userMgrPhone().type(PHONE_NUMBER);
+  ePortOrgAdminPage.userMgrEmail().type(PRIMARY_EMAIL);
+  ePortOrgAdminPage.userMgrTypeDropdown().select(USER_TYPE);
+  ePortOrgAdminPage.userMgrLoginName().type(LOGIN_NAME);
+  ePortOrgAdminPage.userMgrNewPass().type(NEW_PASSWORD);
+  ePortOrgAdminPage.userMgrAvailOrgs().select(ORGANIZATION);
+  ePortOrgAdminPage.userMgrAddBtn().click();
+  ePortOrgAdminPage.userMgrSelectedOrgs().should("contain.text", ORGANIZATION);
+  ePortOrgAdminPage.userMgrCreateBtn().click();
+  cy.screenshot();
+  ePortOrgAdminPage
+    .userMgrMessage()
+    .should("have.text", `User ${LOGIN_NAME} has been created successfully.`);
+  cy.screenshot();
 });
 
-Given('Verify new user created', () => {
-    ePortOrgAdminPage.tabUserMgr().as('tabUserMgr').click()
-    cy.get('@tabUserMgr')
-        .should('have.text', 'User Manager')
-        .should('have.class', 'selectedtabbutton');
-    ePortOrgAdminPage.searchOrganization().select(ORGANIZATION.split(' ')[0]);
-    ePortOrgAdminPage.userMgrSearchOffice().should('exist');
-    ePortOrgAdminPage.showActiveUserCheckbox().as('showActiveUserCheckbox').check();
-    cy.get('@showActiveUserCheckbox').should('be.checked');
-    ePortOrgAdminPage.userMgrSearchFirstName().type(FIRST_NAME);
-    ePortOrgAdminPage.userMgrSearchLastName().type(LAST_NAME);
-    ePortOrgAdminPage.userMgrSearchClearBtn().should('exist');
-    ePortOrgAdminPage.userMgrSearchBtn().click();
-    ePortOrgAdminPage.userMgrGridEvenRow().should('not.exist');
-    cy.screenshot();
-    ePortOrgAdminPage.userMgrGridOddRow()
-        .should('have.length', 1)
-        .should('contain.text', FIRST_NAME)
-        .should('contain.text', LAST_NAME)
-        .should('contain.text', LOGIN_NAME)
-        .should('contain.text', ORGANIZATION.split(' ')[0])
-        .should('contain.text', USER_TYPE)
-        .should('contain.text', PHONE_NUMBER)
-        .should('contain.text', PRIMARY_EMAIL);
-    ePortOrgAdminPage.userMgrGridActiveCheckboxes()
-        .should('have.length', 1)
-        .should('be.checked');
-    ePortOrgAdminPage.userMgrGridEditBtn()
-        .should('have.length', 1)
-        .should('have.value', 'Edit');
+Given("Verify new user created", () => {
+  ePortOrgAdminPage.tabUserMgr().as("tabUserMgr").click();
+  cy.get("@tabUserMgr")
+    .should("have.text", "User Manager")
+    .should("have.class", "selectedtabbutton");
+  ePortOrgAdminPage.searchOrganization().select(ORGANIZATION.split(" ")[0]);
+  ePortOrgAdminPage.userMgrSearchOffice().should("exist");
+  ePortOrgAdminPage
+    .showActiveUserCheckbox()
+    .as("showActiveUserCheckbox")
+    .check();
+  cy.get("@showActiveUserCheckbox").should("be.checked");
+  ePortOrgAdminPage.userMgrSearchFirstName().type(FIRST_NAME);
+  ePortOrgAdminPage.userMgrSearchLastName().type(LAST_NAME);
+  ePortOrgAdminPage.userMgrSearchClearBtn().should("exist");
+  ePortOrgAdminPage.userMgrSearchBtn().click();
+  ePortOrgAdminPage.userMgrGridEvenRow().should("not.exist");
+  cy.screenshot();
+  ePortOrgAdminPage
+    .userMgrGridOddRow()
+    .should("have.length", 1)
+    .should("contain.text", FIRST_NAME)
+    .should("contain.text", LAST_NAME)
+    .should("contain.text", LOGIN_NAME)
+    .should("contain.text", ORGANIZATION.split(" ")[0])
+    .should("contain.text", USER_TYPE)
+    .should("contain.text", PHONE_NUMBER)
+    .should("contain.text", PRIMARY_EMAIL);
+  ePortOrgAdminPage
+    .userMgrGridActiveCheckboxes()
+    .should("have.length", 1)
+    .should("be.checked");
+  ePortOrgAdminPage
+    .userMgrGridEditBtn()
+    .should("have.length", 1)
+    .should("have.value", "Edit");
 });
--- E2E/ePort/cypress/integration/step-definitions/new-sbms-estimate-eport-workflow/new-sbms-estimate-eport-workflow.js
+++ E2E/ePort/cypress/integration/step-definitions/new-sbms-estimate-eport-workflow/new-sbms-estimate-eport-workflow.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 /// <reference types="cypress-iframe" />
-import 'cypress-iframe';
+import "cypress-iframe";
 import { Given } from "@badeball/cypress-cucumber-preprocessor";
 
 import EnvUtils from "../../../support/utils/EnvUtils";
@@ -8,128 +8,153 @@ import ElevenLoginPage from "../../../support/page-objects/eleven-pages/ElevenLo
 import ElevenHomePage from "../../../support/page-objects/eleven-pages/ElevenHomePage";
 import EPortHomePage from "../../../support/page-objects/eport-pages/EPortHomePage";
 
-const envUtils = new EnvUtils;
-const elevenLoginPage = new ElevenLoginPage;
-const elevenHomePage = new ElevenHomePage;
-const ePortHomePage = new EPortHomePage;
-const estNumFilePath = 'cypress/fixtures/sbms-estimate/estimate-number.txt'
+const envUtils = new EnvUtils();
+const elevenLoginPage = new ElevenLoginPage();
+const elevenHomePage = new ElevenHomePage();
+const ePortHomePage = new EPortHomePage();
+const estNumFilePath = "cypress/fixtures/sbms-estimate/estimate-number.txt";
 
-Given('Create new SBMS estimate and send to Eleven', () => {
-    cy.sbms('SendNewTvEstimateToEleven');
+Given("Create new SBMS estimate and send to Eleven", () => {
+  cy.sbms("SendNewTvEstimateToEleven");
 });
 
-Given('Verify new SBMS estimate is created', () => {
-    cy.readFile(estNumFilePath).should('include', 'New Estimate')
-    cy.readFile(estNumFilePath).then(($estNum) => {
-        $estNum = $estNum.split('-').pop()
-        cy.writeFile(estNumFilePath, $estNum)
-        cy.readFile(estNumFilePath).should('eq', $estNum)
-    });
+Given("Verify new SBMS estimate is created", () => {
+  cy.readFile(estNumFilePath).should("include", "New Estimate");
+  cy.readFile(estNumFilePath).then(($estNum) => {
+    $estNum = $estNum.split("-").pop();
+    cy.writeFile(estNumFilePath, $estNum);
+    cy.readFile(estNumFilePath).should("eq", $estNum);
+  });
 });
 
-Given('Login to Eleven', () => {
-    cy.visit(envUtils.getElevenUrl())
-    cy.title().should('eq', 'Eleven: Login')
-    cy.screenshot();
-    elevenLoginPage.usernameBox().type(envUtils.getElevenUsername());
-    elevenLoginPage.passwordBox().type(envUtils.getElevenPassword());
-    elevenLoginPage.loginButton().click();
-    cy.title().should('eq', 'Eleven');
-    cy.screenshot();
+Given("Login to Eleven", () => {
+  cy.visit(envUtils.getElevenUrl());
+  cy.title().should("eq", "Eleven: Login");
+  cy.screenshot();
+  elevenLoginPage.usernameBox().type(envUtils.getElevenUsername());
+  elevenLoginPage.passwordBox().type(envUtils.getElevenPassword());
+  elevenLoginPage.loginButton().click();
+  cy.title().should("eq", "Eleven");
+  cy.screenshot();
 });
 
-Given('Search for order in Eleven', () => {
-    var outerIndex = 1;
-    cy.readFile(estNumFilePath).then(($estimateNumber) => {
-        const waitForEstimate = () => {
-            var innerIdex = 1;
-            elevenHomePage.orderSearchBox().type($estimateNumber);
-            elevenHomePage.goButton().click();
-            const waitForOrder = () => {
-                cy.wait(500)
-                cy.is_element_exists(elevenHomePage.headerEstimateTextSyntax()).then($elementExists => {
-                    if ($elementExists === true) {
-                        innerIdex = 10;
-                    } else if (innerIdex < 10 && $elementExists === false) {
-                        innerIdex++;
-                        waitForOrder();
-                    }
-                })
+Given("Search for order in Eleven", () => {
+  var outerIndex = 1;
+  cy.readFile(estNumFilePath).then(($estimateNumber) => {
+    const waitForEstimate = () => {
+      var innerIdex = 1;
+      elevenHomePage.orderSearchBox().type($estimateNumber);
+      elevenHomePage.goButton().click();
+      const waitForOrder = () => {
+        cy.wait(500);
+        cy.is_element_exists(elevenHomePage.headerEstimateTextSyntax()).then(
+          ($elementExists) => {
+            if ($elementExists === true) {
+              innerIdex = 10;
+            } else if (innerIdex < 10 && $elementExists === false) {
+              innerIdex++;
+              waitForOrder();
             }
-            waitForOrder();
-            cy.is_element_exists(elevenHomePage.headerEstimateTextSyntax()).then($elementExists => {
-                if ($elementExists === true) {
-                    outerIndex = 10;
-                } else if (outerIndex < 10 && $elementExists === false) {
-                    outerIndex++;
-                    cy.wait(5000)
-                    cy.reload()
-                    waitForEstimate();
-                }
-            });
+          }
+        );
+      };
+      waitForOrder();
+      cy.is_element_exists(elevenHomePage.headerEstimateTextSyntax()).then(
+        ($elementExists) => {
+          if ($elementExists === true) {
+            outerIndex = 10;
+          } else if (outerIndex < 10 && $elementExists === false) {
+            outerIndex++;
+            cy.wait(5000);
+            cy.reload();
+            waitForEstimate();
+          }
         }
-        waitForEstimate();
-        elevenHomePage.headerEstimateText().should('contain.text', $estimateNumber);
-        cy.screenshot();
-    })
-});
-
-Given('Verify TV order status is {string}', $orderStatus => {
-    elevenHomePage.toggleAll().click()
-    elevenHomePage.orderStatus().should('include.text', $orderStatus)
+      );
+    };
+    waitForEstimate();
+    elevenHomePage.headerEstimateText().should("contain.text", $estimateNumber);
     cy.screenshot();
+  });
 });
 
-Given('Send order to ePort', () => {
-    elevenHomePage.showChangeAEStationLink().click();
-    elevenHomePage.selectSellerDropdown().select(envUtils.getSeller());
-    elevenHomePage.selectViaDropdown().select(envUtils.getVia());
-    elevenHomePage.selectSubRepCoDropdown().select(envUtils.getSubRepCompany());
-    elevenHomePage.selectSubRepOfficeDropdown().select(envUtils.getSubRepOffice());
-    elevenHomePage.selectChangeAEDropdown()
-        .select(`${envUtils.getEPortUserLastName()}, ${envUtils.getEPortUserFirstName()} - spot seller`);
-    elevenHomePage.okButton().click();
-    elevenHomePage.lstActionName().select('Send Selected Unsent Order(s)');
-    elevenHomePage.selectAllCheckbox().check()
-    elevenHomePage.submitButton().click();
-    elevenHomePage.okButton().click();
-    cy.screenshot();
+Given("Verify TV order status is {string}", ($orderStatus) => {
+  elevenHomePage.toggleAll().click();
+  elevenHomePage.orderStatus().should("include.text", $orderStatus);
+  cy.screenshot();
 });
 
-Given('Verify new order details in ePort', () => {
-    cy.readFile(estNumFilePath).then(($estimateNumber) => {
-        let startIndex = 0;
-        let endIndex = 120;
-        const waitForEstimate = () => {
-            ePortHomePage.dateReceivedHeader().find('img').invoke('attr', 'alt').then((altText) => {
-                if (altText !== 'Desc') {
-                    ePortHomePage.dateReceivedHeader().click();
-                }
-            });
-            ePortHomePage.firstGridRow().invoke('text').then(($firstRowText) => {
-                if (!$firstRowText.includes($estimateNumber) && startIndex < endIndex) {
-                    cy.log(`Waiting on  ${$estimateNumber} order ${(startIndex + 1) * 5} seconds`)
-                    cy.wait(5000);
-                    ePortHomePage.refreshBtn().click();
-                    startIndex++;
-                    waitForEstimate();
-                }
-            });
-        };
-        waitForEstimate();
+Given("Send order to ePort", () => {
+  elevenHomePage.showChangeAEStationLink().click();
+  elevenHomePage.selectSellerDropdown().select(envUtils.getSeller());
+  elevenHomePage.selectViaDropdown().select(envUtils.getVia());
+  elevenHomePage.selectSubRepCoDropdown().select(envUtils.getSubRepCompany());
+  elevenHomePage
+    .selectSubRepOfficeDropdown()
+    .select(envUtils.getSubRepOffice());
+  elevenHomePage
+    .selectChangeAEDropdown()
+    .select(
+      `${envUtils.getEPortUserLastName()}, ${envUtils.getEPortUserFirstName()} - spot seller`
+    );
+  elevenHomePage.okButton().click();
+  elevenHomePage.lstActionName().select("Send Selected Unsent Order(s)");
+  elevenHomePage.selectAllCheckbox().check();
+  elevenHomePage.submitButton().click();
+  elevenHomePage.okButton().click();
+  cy.screenshot();
+});
 
-        cy.getFormattedDate().then(($date) => {
-            ePortHomePage.firstGridRow()
-                .should('contain.text', $date)
-                .should('contain.text', $estimateNumber);
-            ePortHomePage.firstGridRow().contains('New').click();
-            cy.screenshot();
-            ePortHomePage.auditTrailText()
-                .should('contain.text', $date)
-                .should('contain.text', $estimateNumber)
-                .should('contain.text', 'ePort has received new order');
-            cy.screenshot();
-            ePortHomePage.returnButton().click();
+Given("Verify new order details in ePort", () => {
+  cy.readFile(estNumFilePath).then(($estimateNumber) => {
+    let startIndex = 0;
+    let endIndex = 120;
+    const waitForEstimate = () => {
+      ePortHomePage
+        .dateReceivedHeader()
+        .find("img")
+        .invoke("attr", "alt")
+        .then((altText) => {
+          if (altText !== "Desc") {
+            ePortHomePage.dateReceivedHeader().click();
+          }
         });
+      ePortHomePage
+        .firstGridRow()
+        .invoke("text")
+        .then(($firstRowText) => {
+          if (
+            !$firstRowText.includes($estimateNumber) &&
+            startIndex < endIndex
+          ) {
+            cy.log(
+              `Waiting on  ${$estimateNumber} order ${
+                (startIndex + 1) * 5
+              } seconds`
+            );
+            cy.wait(5000);
+            ePortHomePage.refreshBtn().click();
+            startIndex++;
+            waitForEstimate();
+          }
+        });
+    };
+    waitForEstimate();
+
+    cy.getFormattedDate().then(($date) => {
+      ePortHomePage
+        .firstGridRow()
+        .should("contain.text", $date)
+        .should("contain.text", $estimateNumber);
+      ePortHomePage.firstGridRow().contains("New").click();
+      cy.screenshot();
+      ePortHomePage
+        .auditTrailText()
+        .should("contain.text", $date)
+        .should("contain.text", $estimateNumber)
+        .should("contain.text", "ePort has received new order");
+      cy.screenshot();
+      ePortHomePage.returnButton().click();
     });
+  });
 });
